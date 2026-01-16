cf9f7673d18260d0e21f0b860247763e11191f2d
Edgar Vasli <edgar4443@live.com>
2026-01-16 13:10:20 +0200

    Implement soft delete for all entities with global query filters

---
+++ DAL/Migrations/20260116110227_AddSoftDeleteToAllEntities.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftDeleteToAllEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Rooms",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Rooms",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Instructors",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Instructors",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Equipment",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Equipment",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Instructors");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Instructors");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Equipment");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Equipment");
+        }
+    }
+}
---
+++ DAL/Migrations/20260116110513_AddSoftDeleteToBooking.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftDeleteToBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Bookings",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Bookings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Bookings");
+        }
+    }
+}
